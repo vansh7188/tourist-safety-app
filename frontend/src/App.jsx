ed714b1d5c405da593e0817798ae9645c16cc744
varungithubhatt <varunextra993@gmail.com>
2026-04-03 18:14:45 +0530

    my local changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,8 +11,6 @@ import DigitalidForm from "./components/DigitalidForm";
 import { TravelProvider } from "./context/TravelContext";
 import Chatbot from "./components/Chatbot";
 
-import AdminPanel from "./pages/AdminPanel";
-
 function App() {
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
@@ -30,7 +28,6 @@ function App() {
           <Route path="digitalidform" element={<DigitalidForm />} />
           <Route path="digitalid/edit" element={<DigitalidForm />} />
           <Route path="chatbot" element={<Chatbot />} />
-          <Route path="admin" element={<AdminPanel />} />
         </Routes>
       </Router>
     </TravelProvider>
